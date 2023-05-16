f9f1c6c66807632534186e0c6a30c1a7ef79e780
Meghan Flaherty <131192974+flaherty-mi@users.noreply.github.com>
2023-05-16 16:48:51 -0700

    Add files via upload

--- searchManager.java
+++ searchManager.java
@@ -2,11 +2,24 @@
 // manager class-with primary methods
 
 
-
+import java.util.*;
 
 
 package searchGenerator;
 
 public class searchManager extends generatorMain {
+
+    
+public void menu() {
+    System.out.println("welcome to the word gen---");
+    System.out.println("select from options below");
+    System.out.println("[g] generate a new word search");
+    System.out.println("[p] print word search");
+    System.out.println("[s] show the solution to the word search");
+    System.out.println("[q] quit program")
     
 }
+
+
+}
+
