ad2bfadbf43511adc4f5605e53cfd314931db3fc
Meghan Flaherty <131192974+flaherty-mi@users.noreply.github.com>
2023-05-21 16:18:19 -0700

    Add files via upload

--- searchManager.java
+++ searchManager.java
@@ -7,25 +7,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import java.io.PrintStream;
+import java.sql.Array;
 import java.io.FileReader;
 
 public class searchManager extends generatorMain{
     
 
-    public searchManager(char[][] wordGrid, ArrayList<String> line) throws FileNotFoundException {
+    public searchManager(char[][] wordGrid, ArrayList<String> line) {
         menu(wordGrid, line);
     }
 
-
-    
-
-   
-
-
-    public void choice(char[][] wordGrid,ArrayList<String> line ) throws FileNotFoundException {
+    public void choice(char[][] wordGrid,ArrayList<String> line )  {
         Scanner input = new Scanner(System.in);
         String choice = input.next();
     
@@ -48,7 +44,7 @@ public class searchManager extends generatorMain{
         }
     } // end choice
 
-    public void inputOrText(char[][] wordGrid, ArrayList<String> line) throws FileNotFoundException {
+    public void inputOrText(char[][] wordGrid, ArrayList<String> line) {
 
         System.out.println("do you want to input words for a word search, or upload a text file? ");
         System.out.println("type");
@@ -68,7 +64,7 @@ public class searchManager extends generatorMain{
             }
     }  // end inputOrText
 
-    public void createFile(char[][] wordGrid, ArrayList<String> line) throws FileNotFoundException {
+    public void createFile(char[][] wordGrid, ArrayList<String> line)  {
         Scanner input = new Scanner(System.in);
         System.out.print("please enter an input file: ");
         String name;
@@ -77,9 +73,20 @@ public class searchManager extends generatorMain{
         System.out.print("please enter an output file: ");
         String outname = input.next();
         File complete = new File(outname);
-        PrintStream output = new PrintStream(complete);
+        try {
+            PrintStream output = new PrintStream(complete);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
        
-           Scanner one = new Scanner(in);
+           Scanner one = new Scanner(System.in);
+        try {
+            one = new Scanner(in);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
            
            while (one.hasNextLine()) {
             String word = one.nextLine();
@@ -93,7 +100,7 @@ public class searchManager extends generatorMain{
 
         System.out.println("if you would like to re-enter your words please type [r]");
         System.out.println("if no changes are needed please type [n]");
-        String pick = in.next();
+        String pick = one.next();
         switch(pick.toLowerCase()) {
             case "r":
                 line.removeAll(line);
@@ -109,8 +116,11 @@ public class searchManager extends generatorMain{
         
     } // end createFile
 
-    public void createInput(char[][] wordGrid, ArrayList<String> line) throws FileNotFoundException {
+    public void createInput(char[][] wordGrid, ArrayList<String> line) {
+        // if prompted again -> clear que for input
+        line.removeAll(line);
 
+        // scanner + choose # of words to input
         Scanner in = new Scanner(System.in);
         System.out.println("How many words do you want to use?");
         System.out.println("maximum: 7");
@@ -121,6 +131,7 @@ public class searchManager extends generatorMain{
             for (int i = 1; i < wordNum + 1; i++) {
                 System.out.print(i + ": ");
                 word = in.next();
+                word = word.toUpperCase();
                 line.add(word);
             }
         System.out.println();
@@ -144,21 +155,13 @@ public class searchManager extends generatorMain{
                 "solution, choose from menu ");
                 System.out.println();
         }
-
-     // print array here 
-     // prompt if they want to keep the words or use diff ones
-     // if new -> reprompt method
-     // if not -> present options
-
         menuTwo(wordGrid, line);
     } // end createInput
 
 
-    public void printIt(char[][] wordGrid, ArrayList<String> line) throws FileNotFoundException {
+    public void printIt(char[][] wordGrid, ArrayList<String> line)  {
         // should include randomizer for char letters outside arraylist
         
-        
-
         for (int i = 0; i < wordGrid.length; i++) {
             for (int j = 0; j < wordGrid[i].length; j++) {
                 String letters = "ABCDEFGHIJKLMNOP";
@@ -168,15 +171,18 @@ public class searchManager extends generatorMain{
 
                 if (j == 29) {
                 System.out.println(c);
-                }
+                } 
             }  
         }
         menuTwo(wordGrid, line);
     } // end printIt
 
-    public void Solution(char[][] wordGrid, ArrayList<String> line) throws FileNotFoundException {
+    public void Solution(char[][] wordGrid, ArrayList<String> line) { 
+       for(int x = 0; x < line.size(); x++) {
+         //for(int y = 0; y < line; )
+       }
 
-        for (int i = 0; i < wordGrid.length; i++) {
+        for (int i = 0; i < wordGrid.length; i++) { 
             for (int j = 0; j < wordGrid[i].length; j++) {
                 System.out.print('X');
 
@@ -188,7 +194,7 @@ public class searchManager extends generatorMain{
         menuTwo(wordGrid, line);
     } // end Solution
 
-    public void menu(char[][] wordGrid, ArrayList<String> line) throws FileNotFoundException {
+    public void menu(char[][] wordGrid, ArrayList<String> line)  {
         System.out.println("welcome to the word search game!");
         System.out.println("select from options below");
         System.out.println("[g] generate a new word search");
@@ -198,7 +204,8 @@ public class searchManager extends generatorMain{
     choice(wordGrid, line);
     } // end menu
 
-    public void menuTwo(char[][] wordGrid, ArrayList<String> line) throws FileNotFoundException {
+    public void menuTwo(char[][] wordGrid, ArrayList<String> line)  {
+        System.out.println();
         System.out.println("would you like to do anything else?");
         System.out.println();
         System.out.println("select from options below");
