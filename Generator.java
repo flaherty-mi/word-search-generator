7f57b6d792116e3663f7f993ac90305823136f1f
Meghan Flaherty <131192974+flaherty-mi@users.noreply.github.com>
2023-05-22 21:04:54 -0700

    Update Generator.java
    didn't need this class-will try to delete

--- Generator.java
+++ Generator.java
@@ -2,26 +2,5 @@
 // CS 145
 // Constructor
 
-
- // package searchGenerator;
-
-// import java.lang.reflect.Array;
-
-public class Generator extends generatorMain {
-
-    public Generator(char[][] wordGrid) {
-
-       // wordGrid [15][30];
-
-      /*   for (int i = 0; i < wordGrid.length; i++) {
-            for (int j = 0; j < wordGrid[i].length; j++) {
-                
-                System.out.print('X');
-
-                if (j == 29) {
-                  System.out.println('X');
-                }
-            }  
-        } */
-    }   
-}
+// did not end up needing this class
+ 
